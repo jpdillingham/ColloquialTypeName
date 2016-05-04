87d5ac5a6477e106acbf96e3494c76642cf75879
JP Dillingham <jpdillingham@gmail.com>
2016-05-04 18:51:21 -0500

    Update GetColloquialTypeName.cs
    Added the extension method

--- GetColloquialTypeName.cs
+++ GetColloquialTypeName.cs
@@ -4,7 +4,13 @@
 /// </summary>
 /// <param name="type">The type for which the colloquial name should be created.</param>
 /// <returns>A "pretty" string representation of the provided Type.</returns>
-private static string GetColloquialTypeName(Type type)
+public static string GetColloquialTypeName(Type type)
 {
     return (!type.IsGenericType ? type.Name : type.Name.Split('`')[0] + "<" + String.Join(", ", type.GetGenericArguments().Select(a => GetColloquialTypeName(a))) + ">");
+}
+
+// as an extension method
+public static string ToColloquialString(this Type type)
+{
+    return (!type.IsGenericType ? type.Name : type.Name.Split('`')[0] + "<" + String.Join(", ", type.GetGenericArguments().Select(a => a.ToColloquialString())) + ">");
 }
